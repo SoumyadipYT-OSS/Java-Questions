cf44d100decf6c89fa90c3fbc9496daf78868d6b
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2022-09-27 14:28:52 +0530

    Add files via upload

---
+++ Basic Part-I/Q3.java
@@ -0,0 +1,14 @@
+package Basic_Part_1;
+
+/*
+ * Write a Java program to divide two numbers and print on the screen
+ */
+public class Q3 
+{
+	public static void main(String[] args) 
+	{
+		int a = 10;
+		int b = 2;
+		System.out.println("Division: "+(a/b));
+	}
+}
