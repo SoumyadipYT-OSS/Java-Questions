1fad89bd9a92fc2ce3799617b93aa1dcc4f8caa5
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2022-09-27 14:28:31 +0530

    Add files via upload

---
+++ Basic Part-I/Q2.java
@@ -0,0 +1,14 @@
+package Basic_Part_1;
+
+/*
+ * Write a Java program to print the sum of two numbers
+ */
+public class Q2 
+{
+	public static void main(String[] args) 
+	{
+		int a = 80;
+		int b = 60;
+		System.out.println("Sum: "+(a+b));
+	}
+}
