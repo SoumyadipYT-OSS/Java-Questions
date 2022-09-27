55b3e03e13c3fda974d8a66cd7af1df14f20599d
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2022-09-27 16:54:54 +0530

    Add files via upload

---
+++ Basic Part-I/Q10.java
@@ -0,0 +1,14 @@
+package Basic_Part_1;
+
+/*
+ * Write a Java program to compute a specified formula:
+ 		→ 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11))
+*/
+public class Q10 
+{
+	public static void main(String[] args) 
+	{
+		System.out.print("Output: ");
+		System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+	}
+}
