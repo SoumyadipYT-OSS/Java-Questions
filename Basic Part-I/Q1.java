1fbf02e8cdad24ea40c427863f95d7b9df1450d4
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2022-09-27 14:27:55 +0530

    Add files via upload

---
+++ Basic Part-I/Q1.java
@@ -0,0 +1,14 @@
+package Basic_Part_1;
+
+/*
+ * Write a Java program to print 'Hello' on screen and 
+ * then print your name on a separate line.
+ */
+public class Q1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello");
+		System.out.println("Soumyadip Majumder");
+	}
+}
