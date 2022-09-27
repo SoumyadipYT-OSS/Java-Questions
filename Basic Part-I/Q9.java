dad7af75139f0e4c782a8e6572fd31c715e205fb
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2022-09-27 16:54:37 +0530

    Add files via upload

---
+++ Basic Part-I/Q9.java
@@ -0,0 +1,14 @@
+package Basic_Part_1;
+
+/*
+ * Write a Java program to compute the specified 
+ * expressions and print the output:
+ 	→ ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5))
+ */
+public class Q9 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println((25.5*3.5-3.5*3.5)/(40.5-4.5));
+	}
+}
