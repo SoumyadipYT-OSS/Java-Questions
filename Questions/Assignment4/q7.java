b962780dd361f70e1b7eb2bde0b278dec501b340
Soumyadip Majumder <94290293+SoumyadipYT-OSS@users.noreply.github.com>
2023-08-05 19:21:22 +0530

    Add files via upload

---
+++ Questions/Assignment4/q7.java
@@ -0,0 +1,22 @@
+package Important_Questions_in_java.Assignment4;
+/* Linear Search Algorithm */
+public class q7 {
+    public static int linearSearch(int[] arr, int x) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == x)
+                return i;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, 4, 10, 40 };
+        int x = 10;
+        int result = linearSearch(arr, x);
+        if (result == -1)
+            System.out.println("Element is not present in array");
+        else
+            System.out.println("Element is present at index " + result);
+    }
+}
---
+++ Questions/Assignment4/q8.java
@@ -0,0 +1,24 @@
+package Important_Questions_in_java.Assignment4;
+
+/* Bubble sort algorithm */
+public class q8 {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        bubbleSort(arr);
+        System.out.println("Sorted array");
+        for (int j : arr) System.out.print(j + " ");
+    }
+}
